bb331202b948f85fedf3be0144850ce4bc0a5630
Fabio Luporini <f.luporini12@imperial.ac.uk>
2013-11-20 11:05:14 +0000

    Code clean up

--- pyop2/host.py
+++ pyop2/host.py
@@ -116,13 +116,6 @@ class Arg(base.Arg):
              'dim': self.data.split[i].cdim,
              'off': ' + %d' % j if j else ''}
 
-    def c_ind_flatten_data(self, i):
-        return "%(name)s + %(map_name)s[i * %(arity)s + i_0 %% %(arity)d] * %(dim)s + (i_0 / %(arity)d)" % \
-                {'name': self.c_arg_name(),
-                 'map_name': self.c_map_name(0, i),
-                 'arity': self.map.arity,
-                 'dim': self.data.cdim}
-
     def c_ind_data_xtr(self, idx, i, j=0):
         return "%(name)s + xtr_%(map_name)s[%(idx)s] * %(dim)s%(off)s" % \
             {'name': self.c_arg_name(),
@@ -400,7 +393,6 @@ class JITModule(base.JITModule):
         _const_decs = '\n'.join([const._format_declaration()
                                 for const in Const._definitions()]) + '\n'
 
-        #from IPython import embed; embed()
         self._dump_generated_code(code_to_compile)
         # We need to build with mpicc since that's required by PETSc
         cc = os.environ.get('CC')
